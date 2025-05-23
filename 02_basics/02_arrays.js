1becb1a2a1ce5d0cb31e42e443c40b38e5dce030
Md Altamash Iqbal <altamashiqbal212@gmail.com>
2025-05-23 17:58:15 +0000

    02_arrays.js

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,7 @@
+const marvel_heroes = ["thor", "Ironman", "spiderman"]
+const dc = ["Superman", "flash", "batman"]
+
+marvel_heroes.push(dc_heroes)
+
+console.log(marvel_heroes);
+console.log(marvel_heroes[3][1]);
